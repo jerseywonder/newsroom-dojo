df48789af537e93cca108adb951c1ca0cefe81b5
Andy Ball <work@andyball.info>
2024-06-06 13:03:06 +1000

    Change output structure

--- rollup.config.js
+++ rollup.config.js
@@ -4,23 +4,39 @@ import { babel } from '@rollup/plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 
-export default {
-  input: 'src/index.js',
-  output: [
-    {
-      file: 'dist/newsroom-dojo.cjs.js',
-      format: 'cjs'
-    },
-    {
-      file: 'dist/newsroom-dojo.esm.js',
-      format: 'esm'
-    }
-  ],
-  plugins: [
-    resolve(),
-    commonjs(),
-    babel({ babelHelpers: 'bundled' }),
-    terser(),
-    json()
-  ]
-};
+export default [
+  {
+    input: 'src/arrays/index.js',
+    output: [
+      {
+        file: 'dist/arrays/index.js',
+        format: 'es',
+        name: 'arrays'
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({ babelHelpers: 'bundled' }),
+      terser(),
+      json()
+    ]
+  },
+  {
+    input: 'src/schema/index.js',
+    output: [
+      {
+        file: 'dist/schema/index.js',
+        format: 'es',
+        name: 'schema'
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonjs(),
+      babel({ babelHelpers: 'bundled' }),
+      terser(),
+      json()
+    ]
+  }
+];
